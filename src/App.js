3abe1105db8fa84b8eff211cf28c25bad6968d12
Daniel Foster <danieljfoster87@gmail.com>
2021-03-11 13:36:49 -0500

    updated logos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './Images/logo.jpeg';
+import smLogoWhite from './Images/L&S_White_500px.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p className="text-logo">L<span className="red">&</span>S</p>
+        <img src={ smLogoWhite } alt="L&S" className="logo-sm-white"></img>
 
         <div className="top-links">
-          <a href="tel:2015721516">Sal: (201) 572-1516</a>
-          <a href="tel:9738760921">Luis: (973) 876-0921</a>
+          <a href="tel:2015721516"><span className="red red-text-border">Sal:</span> (201) 572-1516</a>
+          <a href="tel:9738760921"><span className="red">Luis:</span> (973) 876-0921</a>
         </div>
       </header>
       <main>
